7711fb04a08575b5142f0c4cea6e1fe94ccf3faf
PandaBump <stokesk6128@gmail.com>
2022-09-30 18:30:01 -0400

    Project3_math_operators Interactive js file
    Current development of operators project

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/Project3_math_operators.js
@@ -0,0 +1,31 @@
+var z = function.mathRandom();
+var x = window.prompt("What is the first number?");
+var y = window.prompt("What is the second number?");
+alert("Calculating...");
+function calculatingAdd(x, y) {
+    return var 1 (x + x);
+  }
+  function calculatingSub(x, y) {
+    return x - x;
+  }
+  function calculatingMulti(x, y) {
+    return x * x;
+  }
+  function calculatingRemain(x, y) {
+    return x % x;
+  }
+  function calculatingIncre(x, y) {
+    return x ++ x;
+  }
+  function calculatingDecre(x, y) {
+    return x -- x;
+  }
+function showResults{
+    document.write("Your numbers added are: " + calculatingAdd);
+    document.write("Your numbers subtracted are: " + calculatingSub);
+    document.write("Your numbers multiplied are: " + calculatingMulti);
+    document.write("The remainder of your numbers are: " + calculatingRemain);
+    document.write("The increments of your numbers are: " + calculatingIncre);
+    document.write("The decrements of your numbers are: " + calculatingDecre);
+    document.write("Next time, try using the number " + z + " in your choices.");
+}
--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -2,7 +2,7 @@ var X = "Don't click that 'OK' button, scoundrel!"; var Y = "Engarde, " +" ninny
 var Sent1 = "This is the beginning of the string"; var Sent2 = "and this is the end of the string";
 //Interactive Browser Game//
 window.alert(X);
-window.alert(Y)
+window.alert(Y);
 document.write(Sent1)+(Sent2);
 document.write(3+3);
 function myFunction() {
@@ -10,3 +10,8 @@ function myFunction() {
     sentence += " a lot from this book!";
     document.getElementById("concatenate") .innerHTML = sentence;
 }
+function.mathRandom( )
+var x = window.prompt("What is the first number?");
+var y = window.prompt("What is the second number?");
+alert("Calculating...");
+document.write("Your answer is " + (x+y));
